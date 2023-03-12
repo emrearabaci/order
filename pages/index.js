ae6dffbdfc8ef1c607ee4e5dd5911f2f721937e2
Emre Arabaci <a.emrearabaci@gmail.com>
2023-03-12 22:20:03 +0300

    Font and Head configs moved to "Layout" file.

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,16 @@ import Head from 'next/head';
 /* Styles */
 import styles from '@/styles/pages/Index.module.scss';
 
-/* Fonts */
-import { Inter } from 'next/font/google';
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
     <>
       <Head>
-        <title>order</title>
-        <meta name="description" content="quick order" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/*
-        <link rel="icon" href="/favicon.ico" />
-        */}
+        <title>Homepage | Order</title>
       </Head>
-      <main className={inter.className}>
-        <div className={styles.container}>
-          <p>Order</p>
-        </div>
-      </main>
+
+      <div className={styles.container}>
+        <p>Order</p>
+      </div>
     </>
   );
 }
