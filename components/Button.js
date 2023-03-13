e31a7682f5a7e11ef391265088e28630be72c4ff
Emre Arabaci <a.emrearabaci@gmail.com>
2023-03-13 22:58:45 +0300

    Created "button" component.

---
+++ components/Button.js
@@ -0,0 +1,10 @@
+/* Styles */
+import styles from '@/styles/components/Button.module.scss';
+
+export default function Button({ type, onClick, content }) {
+  return (
+    <button className={styles.button} type={type} onClick={onClick}>
+      {content}
+    </button>
+  );
+}
